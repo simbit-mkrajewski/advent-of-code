92bc14548c44583ccbf80008ca622127a93ab3e0
mkrajewski@aiut.com.pl <mkrajewski@aiut.com.pl>
2015-12-07 14:41:13 +0100

    Create maven project

---
+++ src/main/java/pl/com/simbit/App.java
@@ -0,0 +1,13 @@
+package pl.com.simbit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
